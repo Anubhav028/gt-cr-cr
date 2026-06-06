0455e315165604c65272938238973aafba0067bf
Anubhav Mehrotra <anubhavmehrotra04@gmail.com>
2026-06-06 14:50:48 +0530

    revert something

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function sayName(){
     console.log('My name is ' + name1)
 }
 
+<<<<<<< HEAD
 function newfn(){
     console.log('hey i am a new fn')
+=======
+function isAbove18(){
+    return age>=18
+>>>>>>> parent of fae2c8d (remove 18 fn)
 }
